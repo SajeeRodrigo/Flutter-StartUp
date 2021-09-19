cb0fae0f00fe2a2ededd1e921b2d1ad5fd1eeb95
SajeeRodrigo <sajee.thamanga@gmail.com>
2021-09-20 00:35:04 +0530

    Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,24 @@
 import 'package:flutter/material.dart';
-//flutter package eka athule tyn subpackage eka import krnw
-
-
 
 void main() {
-  //application eke mulinma pennana dewl liynne meke
-  runApp(MyApp());
-  //wiget tika call krnw
+  runApp(myApp());
 }
 
-class MyApp extends StatelessWidget {
-  //class eka dart only meka flutter kiyl gnna thmi inherit krnne flutter class ekak eka import krnw
+class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //widget kiynne layout ekata appbar, text wiget, page wiget, buttons
-    //bulding blocks wage
-    //independent or nested components
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello Sajee!',
-          style: TextStyle(fontSize: 20.0),
-          ),
+          title: Text("Hellow"),
         ),
         body: Center(
-          child: Text('How R u?',
-          style: TextStyle(fontSize: 60.0),
+          child: Text(
+            "Whats Up GUYS??",
+            style: TextStyle(fontSize: 40.0),
           ),
         ),
       ),
-      //home kiynne issellm ape app ekt giyma pennana eka
-
     );
-  //return krnw wiget ekak Materialapp(root element eka) class eke obj ekak hdnw
-  
   }
 }
